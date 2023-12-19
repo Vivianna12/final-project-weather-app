55bd20ccd89bca76229a8ffe3956e2627f8c9732
Vivianna12 <viviannasamite12@gmail.com>
2023-12-19 10:46:11 -0500

    made the search input provide the current temp of the city searched with an api. Also made the page display the current weather for a city when the page is loaded.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+function changeWeatherValue(response) {
+  let tempElement = document.querySelector("#temperature-value");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "f42atbc02aod4017c837cbfe1c70ef22";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(changeWeatherValue);
+}
+
 function handleSearchForm(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  console.log(searchInput.value);
-  let cityElement = document.querySelector("#current-city");
-  console.log(cityElement.innerHTML);
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchForm);
+
+searchCity("Bronx");

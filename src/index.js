a49e401f75433bd8b0515d69f4c3056a0c3d458f
Vivianna12 <viviannasamite12@gmail.com>
2023-12-19 20:42:51 -0500

    replaced the text with api data to make it work and fixed some classes so that everything works

--- src/index.js
+++ src/index.js
@@ -48,7 +48,11 @@ function handleSearchForm(event) {
 
   searchCity(searchInput.value);
 }
-
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  return days[date.getDay()];
+}
 function getForecast(city) {
   let apiKey = "f42atbc02aod4017c837cbfe1c70ef22";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
@@ -56,25 +60,30 @@ function getForecast(city) {
   console.log(apiUrl);
 }
 function displayForecast(response) {
-  console.log(response.data);
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div class="weather-forecast-icon">
+         <img src="${day.condition.icon_url}" />
+        </div>
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
           </span>
-          <span class="weather-forecast-temperature">9º</div>
+          <span class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
         </span>
       </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
